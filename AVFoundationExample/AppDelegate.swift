2ecd51f82a869b4c89d23d3832bf5ff6c61da69d
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2017-04-03 11:04:07 +0200

    Initial Commit

---
+++ AVFoundationExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AVFoundationExample
+//
+//  Created by Friedrich HAEUPL on 03.04.17.
+//  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AVFoundationExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AVFoundationExample
+//
+//  Created by Friedrich HAEUPL on 03.04.17.
+//  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
